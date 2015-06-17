8d5750871cff2011389d2ee51d2196b057c7a4e5
Danny Pranoto <danny.pranoto@veritrans.co.id>
2015-06-17 14:08:21 +0700

    Updated select dropdown style

--- app/shared/form/form.directive.js
+++ app/shared/form/form.directive.js
@@ -184,3 +184,16 @@ return {
     }
 }}])
 
+formApp.directive('select', function($interpolate) {
+  return {
+    restrict: 'E',
+    require: 'ngModel',
+    link: function(scope, elem, attrs, ctrl) {
+      var defaultOptionTemplate;
+      scope.defaultOptionText = attrs.defaultOption || 'Select...';
+      defaultOptionTemplate = '<option value="" disabled selected style="display: none;">{{defaultOptionText}}</option>';
+      elem.prepend($interpolate(defaultOptionTemplate)(scope));
+    }
+  };
+});
+
